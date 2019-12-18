9e128690433c4776cf0b8366e5a303632833532f
Bassie Bangura <bassiebangura@Bassies-MacBook-Pro.local>
2019-12-18 13:06:20 -0500

    Add files.

---
+++ src/flureeFetch.js
@@ -0,0 +1,103 @@
+import fetch from "isomorphic-fetch";
+// import { serverURL } from "./appConfig";
+
+function isDevelopment() {
+	return process.env.NODE_ENV === "development";
+}
+
+function parseJSON(response) {
+	return response.json().then(function(json) {
+		const newResponse = Object.assign(response, { json });
+		if (response.status < 300) {
+			return newResponse;
+		} else {
+			throw newResponse.json;
+		}
+	});
+}
+
+function newToken() {
+	return fetch(`${serverURL}get-fluree-token`, { method: "GET" })
+		.then(parseJSON)
+		.then(res => {
+			let token = res.json.token;
+			localStorage.setItem("trustClarityToken", token);
+			// debugger;
+			return token;
+		})
+		.catch(err => {
+			// debugger;
+			let errorMessage = (err.json && err.json.message) || err;
+			throw new Error(errorMessage);
+		});
+}
+
+function getToken() {
+	return new Promise(function(resolve, reject) {
+		let token = localStorage.getItem("trustClarityToken");
+		if (!token) {
+			// debugger;
+			resolve(newToken());
+		} else {
+			resolve(token);
+		}
+	});
+}
+
+function fetchResp(fullUri, fetchOpts, isDev) {
+	return fetch(fullUri, fetchOpts)
+		.then(parseJSON)
+		.then(resp => resp.json)
+		.catch(err => {
+			let errorMessage =
+				err.message || (err.json && err.json.message) || err.json || err;
+			throw new Error(errorMessage);
+		});
+}
+
+var flureeFetch = (uri, body) => {
+	const isDev = isDevelopment();
+	const gateway = isDev
+		? "http://localhost:8080/fdb"
+		: "https://db.flur.ee/api/db";
+	const db = isDev ? "/test/tcfinal3" : "/tcdemo/test";
+	let fullUri;
+	if (uri === "/new-db") {
+		fullUri = gateway + uri;
+	} else if (!isDev && uri === "/dbs") {
+		fullUri = gateway + "s";
+	} else if (isDev && uri === "/dbs") {
+		fullUri = gateway + "/dbs";
+	} else {
+		fullUri = gateway + db + uri;
+	}
+
+	var fetchOpts = {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(body)
+	};
+
+	return new Promise((resolve, reject) => {
+		if (!isDev) {
+			getToken()
+				.then(res => (fetchOpts.headers["Authorization"] = `Bearer ${res}`))
+				.then(res => fetchResp(fullUri, fetchOpts))
+				.then(res => {
+					console.log(res);
+					if (Object.keys(res).includes("result")) {
+						return resolve(res.result);
+					} else {
+						return resolve(res);
+					}
+				})
+				.catch(err => reject(err));
+		} else {
+			fetchResp(fullUri, fetchOpts)
+				.then(res => resolve(res))
+				.catch(err => reject(err));
+		}
+	});
+};
+
+export { flureeFetch, parseJSON, newToken, getToken };
