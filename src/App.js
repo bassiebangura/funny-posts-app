3b750890d44127a096ee708cebe0110eb82304b9
Bassie Bangura <bassiebangura@Bassies-MacBook-Pro.local>
2019-12-23 18:49:12 -0500

    Working posts and comments for each user.

--- src/App.js
+++ src/App.js
@@ -1,62 +1,30 @@
 import React, { useState, useEffect } from "react";
 import Person from "./models/person";
 import { flureeFetch } from "./flureeFetch";
+
 import "./App.css";
-//query for post by individual and the posts of all the people the individual follows
-// {
-//   "select": [
-//     {
-//       "posts": [
-//         "message",
-//         {
-//           "comments": [
-//              {
-//                 "comment/person": ["fullName"]
-//             },
-//             "message"
-//           ]
-//         }
-//       ]
-//     },
-//     {
-//       "follows": [
-//         {
-//           "posts": [
-//             "message",
-//             {
-//               "comments": [
-//                 {
-//                 "comment/person": ["fullName"]
-//             },
-//             "message"
-//               ]
-//             }
-//           ]
-//         }
-//       ]
-//     }
-//   ],
-//   "from": ["person/handle", "jsuleman"]
-// }
 
 function App() {
-	// const [individualPosts, setIndividualsPosts] = useState([]);
-	// const [followsPostsMessages, setFollowsPosts] = useState([]);
 	const [posts, setPosts] = useState({
-		individualPostsMessages: [],
-		followsPostMessages: []
+		individualPostsAndComments: [],
+		followsPostsAndComments: []
 	});
-	const updatePosts = ({ individualPostsMessages, followsPostMessages }) =>
+	const updatePosts = ({
+		individualPostsAndComments,
+		followsPostsAndComments
+	}) =>
 		setPosts(prevState => ({
 			...prevState,
-			individualPostsMessages,
-			followsPostMessages
+			individualPostsAndComments,
+			followsPostsAndComments
     }));
   
-	const refreshPosts = () => {
+
+
+	const refreshPosts = (id) => {
+    console.log(id)
 		const query = {
 			select: [
-				"fullName",
 				{
 					posts: [
 						"message",
@@ -89,10 +57,10 @@ function App() {
 					]
 				}
 			],
-			from: ["person/handle", "jsuleman"]
-    };
-    
-    let generateArrayOfCommentsMessage = arrayOfComments => {
+			from: parseInt(id)
+		};
+
+		let generateArrayOfCommentsMessage = arrayOfComments => {
 			let mapping = arrayOfComments.map(item => {
 				return item.message;
 			});
@@ -105,50 +73,20 @@ function App() {
 				}
 				const { follows, posts } = res;
 
-				const arrayOfIndividualPosts = posts.map(item => item.message);
-				const individualPostsComments = posts.map(item => item.comments);
-
-
-
 				const individualPostsAndComments = posts.map(item => ({
 					message: item.message,
 					comments: generateArrayOfCommentsMessage(item.comments)
-        }));
-        // (function displayPostAndComments() {
-        //   let mapping = individualPostsAndCommentsObject.map(item => {
-        //     let commentsMessage = []
-        //     item.comments.forEach(element => {
-        //       console.log(element)
-        //        return commentsMessage.push(element.message)
-        //     });
-            
-        //     return (
-        //       commentsMessage
-        //     )
-        //   })
-        //   console.log(mapping)
-        // }
-        // )();
-
-        
+				}));
 
 				const followsPosts = follows.map(item => item.posts);
-				const arrayOfFollowsPosts = followsPosts.map(
-					(item, index) => item[0].message
-				);
-				const testFollowsPosts = followsPosts.map((item, index) => ({
+				const followsPostsAndComments = followsPosts.map((item, index) => ({
 					message: item[0].message,
-					comments: item[0].comments
+					comments: generateArrayOfCommentsMessage(item[0].comments)
 				}));
-				console.log(posts);
-				console.log(individualPostsAndComments);
-				//   console.log(follows);
-				//   console.log(followsPosts);
-				// console.log(testFollowsPosts)
-				//console.log(individualPostsComments)
+
 				updatePosts({
-					individualPostsMessages: arrayOfIndividualPosts,
-					followsPostMessages: arrayOfFollowsPosts
+					individualPostsAndComments,
+					followsPostsAndComments
 				});
 			})
 			.catch(err => {
@@ -157,115 +95,87 @@ function App() {
 			});
 	};
 	useEffect(() => {
-		//refreshPosts();
-	}, [posts]);
+		refreshPosts();
+  }, [setPosts]);
+  
+  const handleSubmit = e => {
+    refreshPosts(e.target.id)
+  }
+  const [users, setUsers] = useState([]);
 
-	// const [CEGroups, setCEGroups] = useState({
-	// 	CEGroups: [],
-	// 	operators: [],
-	// 	updateCEGroups: CEGroups =>
-	// 		setCEGroups(prevState => ({ ...prevState, CEGroups }))
-	// });
-	// useEffect(() => {
-	// 	const query = {
-	// 		select: [
-	// 			"*",
-	// 			{ "CEGroup/products": ["*"] },
-	// 			{ "CEGroup/operators": ["name", "_id"] }
-	// 		],
-	// 		from: "CEGroup"
-	// 	};
-	// 	flureeFetch("/query", query)
-	// 		.then(res => {
-	// 			if (!res) {
-	// 				throw new Error("Error fetching products.");
-	// 			}
-	// 			//const modelCEGroups = res.map(c => new CEGroup(c));
-	// 			//CEGroups.updateCEGroups(modelCEGroups);
-	// 		})
-	// 		.catch(err => {
-	// 			debugger;
-	// 		});
-	// });
+	const getUsers = () => {
+		const query = {
+			select: ["fullName", "handle"],
+			from: "person"
+		};
 
-	// let testEndPoint = () => {
-	// 	let query = {
-	// 		select: [
-	// 			"fullName",
-	// 			{
-	// 				posts: [
-	// 					"message",
-	// 					{
-	// 						comments: [
-	// 							{
-	// 								"comment/person": ["fullName"]
-	// 							},
-	// 							"message"
-	// 						]
-	// 					}
-	// 				]
-	// 			},
-	// 			{
-	// 				follows: [
-	// 					"fullName",
-	// 					{
-	// 						posts: [
-	// 							"message",
-	// 							{
-	// 								comments: [
-	// 									{
-	// 										"comment/person": ["fullName"]
-	// 									},
-	// 									"message"
-	// 								]
-	// 							}
-	// 						]
-	// 					}
-	// 				]
-	// 			}
-	// 		],
-	// 		from: ["person/handle", "jsuleman"]
-	// 	};
+		return flureeFetch("/query", query)
+			.then(res => {
+				if (!res) {
+					throw new Error("Error fetching posts.");
+        }
+				const fullNamesAndId = res.map(item => item);
+				setUsers(fullNamesAndId);
+			})
+			.catch(err => {
+				console.log(err);
+				debugger;
+			});
+	};
+	useEffect(() => {
+		getUsers();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [setUsers]);
 
-	// 	flureeFetch("/query", query)
-	// 		.then(res => {
-	//       let {follows, posts } = res;
-
-	// 			let arrayOfIndividualPosts = posts.map(item => item.message);
-	// 			let followsPosts = follows.map(item => item.posts);
-	// 			let arrayOfFollowsPosts = followsPosts.map(
-	// 				(item, index) => item[0].message
-	// 			);
-
-	// 			console.log(arrayOfIndividualPosts);
-	// 			console.log(arrayOfFollowsPosts);
-
-	// 			if (!res) {
-	// 				throw new Error("Error fetching products.");
-	// 			}
-	// 		})
-	// 		.catch(err => {
-	// 			debugger;
-	// 		});
-
-	// };
-
-	//console.log(posts)
 	return (
 		<div className="App">
 			<header className="App-header">
-				<div>
-					<h1>hello gang</h1>
-					<button onClick={() => refreshPosts()}>Click To Get Post</button>
+				<h1>Funny Post App</h1>
+			</header>
+			<div className="users-and-posts-wrapper">
+				<div className="posts-and-comments-container">
+					{posts.individualPostsAndComments.map(item => (
+						<div className="posts-container">
+							<p>{item.message}</p>
+							<div className="posts-comments">
+								<p className="comments-header">Comments</p>
+								{item.comments.map(item => (
+									<p className="comment-message">{item}</p>
+								))}
+							</div>
+						</div>
+					))}
 
-					{/* {posts.individualPostsMessages.map(item => (
-						<p>{item}</p>
-          ))} */}
-					{posts.followsPostMessages.map(item => (
-						<p>{item}</p>
+					{posts.followsPostsAndComments.map(item => (
+						<div className="posts-container">
+							<p>{item.message}</p>
+							<div className="posts-comments">
+								<p className="comments-header">Comments</p>
+								{item.comments.map(item => (
+									<p className="comment-message">{item}</p>
+								))}
+							</div>
+						</div>
 					))}
 				</div>
-			</header>
+
+				<div className="users-container">
+          {users.map(item => 
+          {
+          return (
+						<button
+							onClick={handleSubmit}
+							id={item._id}
+							key={item._id}
+							className="users-button"
+						>
+							{item.fullName}
+						</button>
+          )
+          }
+          )}
+				</div>
+			</div>
 		</div>
 	);
 }
