56edf2be8f65401312038a558f36e4dc11a42476
Bassie Bangura <bassiebangura@Bassies-MacBook-Pro.local>
2020-01-02 11:59:45 -0500

    Create user profile page.

--- src/screens/UserProfilePage.js
+++ src/screens/UserProfilePage.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, {useContext} from "react";
+import Header from "../components/Header";
+import {useParams, Link} from "react-router-dom"
+import UsersContext from "../context/UsersContext";
 function UserProfilePage() {
+		let users = useContext(UsersContext);
+		let {id} = useParams();
+		let userProfileData = users.filter(item => parseInt(id) === parseInt(item._id))
+		console.log(userProfileData)
 	return (
 		<div >
-			<h1>Funny Post App</h1>
-			<h2>
-				User Profile
-			</h2>
+			<Header />
+			{	
+
+				userProfileData.map(item => {
+					
+							return (
+						<div className="user-profile-page">
+							<h2>{item.fullName}'s Profile</h2>
+							<p>Name: {item.fullName}</p>
+							<p>Handle {item.handle}</p>
+							<p>Age {item.age}</p>
+							<p>Following: {item.follows.map(item => <span>{item.fullName},</span>)}</p>	
+							<button>
+								<Link to={"/users"}>Back</Link>
+							</button>					
+						</div>
+					)
+					}
+				
+				)
+			}
 		</div>
 	);
 }
